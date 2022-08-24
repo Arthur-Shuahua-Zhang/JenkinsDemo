ccaa4dcb90bc93f5359632d74b7b04e68e160b9a
Arthur-Shuahua-Zhang <shaohua.zhang@antra.com>
2022-08-24 19:17:05 -0400

    init

---
+++ src/main/java/com/example/jenkinsdemo/JenkinsDemoApplication.java
@@ -0,0 +1,24 @@
+package com.example.jenkinsdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class JenkinsDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JenkinsDemoApplication.class, args);
+    }
+
+}
+
+@RestController
+class MyController {
+
+    @GetMapping("hello")
+    public String sayHello(){
+        return "Hello, this is a big application";
+    }
+}
